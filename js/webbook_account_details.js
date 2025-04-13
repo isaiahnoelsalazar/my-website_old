53a3f458219501a7fc7bc36e7a6ccd64b9315fb4
saiaa <isaiahnoelsalazar474@gmail.com>
2025-04-13 19:45:04 +0800

    Testing WebBook server (12)

--- js/webbook_account_details.js
+++ js/webbook_account_details.js
@@ -1,3 +1,23 @@
+window.onload = function (){
+    let requestDetails = new XMLHttpRequest();
+    requestDetails.open("GET", "https://sasasaia.pythonanywhere.com/details", true);
+    requestDetails.withCredentials = true;
+    requestDetails.onreadystatechange = function (){
+        if (requestDetails.status == 200 && requestDetails.readyState == 4){
+            let response = requestDetails.responseText;
+            if (response == "Not logged in."){
+                window.location.href = "webbook_login.html";
+            } else {
+                let details = response.split("[sprtr_str]");
+                document.getElementById("fname").value = details[0];
+                document.getElementById("lname").value = details[1];
+                document.getElementById("username").value = details[2];
+                document.getElementById("password").value = details[3];
+            }
+        }
+    }
+    requestDetails.send();
+};
 function save(){
     let requestSave = new XMLHttpRequest();
     requestSave.open("POST", `https://sasasaia.pythonanywhere.com/save?fname=${document.getElementById("fname").value}&lname=${document.getElementById("lname").value}&username=${document.getElementById("username").value}&password=${document.getElementById("password").value}`, true);
