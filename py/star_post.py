23174ea7429f18968be2b2b8a7728053300bc890
saiaa <isaiahnoelsalazar474@gmail.com>
2025-04-21 13:47:35 +0800

    Testing WebBook server (36)

--- py/star_post.py
+++ py/star_post.py
@@ -1,7 +1,6 @@
 result = ""
 global a, b, c, d, e, f
 try:
-    post_id = this_post_id
     if session.get("logged_in", "no") != "no":
         try:
             get_post = WebBookPost.query.get_or_404(int(post_id))
@@ -18,9 +17,9 @@ try:
             get_post.post_uid_likes = "["+",".join(a)+"]"
             db.session.commit()
             result = "Success" + separator_string + ("../resources/star.png" if toadd else "../resources/star_filled.png")
-        except Exception as eee1:
-            result = "Server error: " + repr(eee1)
+        except:
+            result = "Server error"
     else:
         result = "Not logged in."
-except Exception as eee2:
-    result = "Server error: " + repr(eee2)
+except:
+    result = "Server error"
