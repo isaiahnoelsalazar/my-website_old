44556fc9f1bea23712246e66f1f1001e32f1c961
sasa <isaiahnoelsalazar474@gmail.com>
2025-04-27 19:28:28 +0800

    NuGet package test

---
+++ nuget/TestClass.cs
@@ -0,0 +1,10 @@
+﻿namespace mywebsite_nugetpackage
+{
+    public class TestClass
+    {
+        public static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
