c749b4bc14e44c0f4157f7b9a8a4f960521e8fda
saiasaia <isaiahnoelsalazar474@gmail.com>
2025-05-22 00:04:19 +0800

    YT MP3/MP4 test (3)

--- py/ytmp3.py
+++ py/ytmp3.py
@@ -1,6 +1,8 @@
-yt = YouTube("https://www.youtube.com/watch?v=" + url)
+'''yt = YouTube("https://www.youtube.com/watch?v=" + url)
 
 audio = yt.streams.filter(only_audio=True).first()
 audio.download()
 
-result = sendfile(audio.title + ".mp3", as_attachment=True, download_name=audio.title + ".mp3")
+result = send_file(audio.title + ".mp3", as_attachment=True, download_name=audio.title + ".mp3")
+'''
+result = "https://www.youtube.com/watch?v=" + url
