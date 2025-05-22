d417d2991f92fe0cd1a3c51e783f90f8d0f90a4a
saiasaia <isaiahnoelsalazar474@gmail.com>
2025-05-23 01:44:05 +0800

    DOC2PDF test (5)

--- py/doc_to_pdf.py
+++ py/doc_to_pdf.py
@@ -12,17 +12,17 @@ if file and allowed_file(file.filename):
     # Secure the filename and save the uploaded DOCX
     filename = secure_filename(file.filename)
     unique_filename = str(uuid.uuid4()) + "_" + filename
-    docx_path = app.config["UPLOAD_FOLDER"] + "/" + unique_filename
+    docx_path = os.path.join(app.config["UPLOAD_FOLDER"], unique_filename)
     file.save(docx_path)
 
     # Generate a unique name for the output PDF
-    pdf_filename = unique_filename + ".pdf"
-    pdf_path = app.config["CONVERTED_FOLDER"] + "/" + pdf_filename
+    pdf_filename = os.path.splitext(unique_filename)[0] + ".pdf"
+    pdf_path = os.path.join(app.config["CONVERTED_FOLDER"], pdf_filename)
 
     # Convert the DOCX to PDF
     if convert_to_pdf(docx_path, pdf_path):
         # If conversion is successful, offer the PDF for download
-        result = send_file(pdf_path, as_attachment=True, download_name=pdf_filename)
+        result = send_file("/home/sasasaia/" + app.config["CONVERTED_FOLDER"] + "/" + pdf_filename, as_attachment=True, download_name=pdf_filename)
     else:
         result = "<h1>Error: DOCX to PDF conversion failed.</h1><p>Please check server logs for more details.</p>"
 else:
