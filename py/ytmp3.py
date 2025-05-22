3660bc26d1a1847c67803855b08773643894188f
saiasaia <isaiahnoelsalazar474@gmail.com>
2025-05-22 19:37:56 +0800

    YT MP3/MP4 removal

--- py/ytmp3.py
+++
@@ -1,6 +0,0 @@
-yt = YouTube("https://www.youtube.com/watch?v=" + url)
-
-audio = yt.streams.filter(only_audio=True).first()
-audio.download()
-
-result = send_file(audio.title + ".mp3", as_attachment=True, download_name=audio.title + ".mp3")
