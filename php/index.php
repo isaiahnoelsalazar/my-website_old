7a2f45e3a8c25a496c9ad316c60ab654e909b982
saiasaia <isaiahnoelsalazar474@gmail.com>
2025-05-26 18:36:16 +0800

    PHP test (6)

---
+++ php/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=7">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#4CAF50">
+        <link rel="apple-touch-icon" sizes="57x57" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="https://saiaaaaaaa.github.io/my-website/resources/icon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="https://saiaaaaaaa.github.io/my-website/resources/icon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://saiaaaaaaa.github.io/my-website/resources/icon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="https://saiaaaaaaa.github.io/my-website/resources/icon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://saiaaaaaaa.github.io/my-website/resources/icon/favicon-16x16.png">
+        <link rel="manifest" href="https://saiaaaaaaa.github.io/my-website/resources/icon/manifest.json">
+        <meta name="msapplication-TileColor" content="#4CAF50">
+        <meta name="msapplication-TileImage" content="https://saiaaaaaaa.github.io/my-website/resources/icon/ms-icon-144x144.png">
+        <title>Sample PHP Pages</title>
+        <!--this works only in Chrome i think -->
+        <script type="speculationrules">
+            {
+                "prefetch": [
+                    {
+                        "urls": [
+                            "http://openweb.fwh.is/sample1.php",
+                            "http://openweb.fwh.is/sample2.php"
+                        ]
+                    }
+                ]
+            }
+        </script>
+        <link rel="stylesheet" href="https://saiaaaaaaa.github.io/my-website/css/colors.css">
+        <link rel="stylesheet" href="https://saiaaaaaaa.github.io/my-website/css/fonts.css">
+        <link rel="stylesheet" href="https://saiaaaaaaa.github.io/my-website/css/index.css">
+        <link rel="stylesheet" href="https://saiaaaaaaa.github.io/my-website/css/pages.css">
+    </head>
+    <body>
+        <div id="navbar">
+            <a class="tab-container">
+                <p class="tab">Sample PHP Pages</p>
+            </a>
+        </div>
+        <div id="main">
+            <div class="pages content">
+                <div id="pages-div-1">
+                    <div id="page-list">
+                        <a href="http://openweb.fwh.is/sample1.php">Sample PHP Page 1</a>
+                        <a href="http://openweb.fwh.is/sample2.php">Sample PHP Page 2</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
